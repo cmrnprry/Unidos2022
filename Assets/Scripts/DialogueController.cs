5a91460cdcb2e47f9a62c470d8273bcce2b3d668
Cam <33362149+cmrnprry@users.noreply.github.com>
2022-09-29 18:41:02 -0400

    fixing it

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -29,12 +29,8 @@ public class DialogueController : MonoBehaviour
     private Coroutine coroutine;
 
     [Header("TextBoxes")]
-    public TextMeshProUGUI friendTextBox;
+    public TextMeshProUGUI textbox;
     public GameObject friend;
-    public TextMeshProUGUI otherTextBox;
-    public GameObject other;
-    public TextMeshProUGUI commentsTextBox;
-    public GameObject comments;
 
     [Header("Dialogue")]
     private bool isTyping;
@@ -53,17 +49,16 @@ public class DialogueController : MonoBehaviour
     {
         if (story.canContinue)
         {
-            RestEffects(friendTextBox);
             string nextLine = story.Continue().Trim();
             currentTags = story.currentTags;
 
-            if (currentTags.Count > 0)
-            {
-                CheckTags();
-            }
+            //if (currentTags.Count > 0)
+            //{
+            //    CheckTags();
+            //}
 
 
-            coroutine = StartCoroutine(incrementText(nextLine, friendTextBox));
+            coroutine = StartCoroutine(incrementText(nextLine, textbox));
         }
         else
         {
@@ -74,103 +69,102 @@ public class DialogueController : MonoBehaviour
 
     }
 
-    private void CheckTags()
-    {
-        foreach (string tag in currentTags)
-        {
-            string[] split = tag.Split(',', StringSplitOptions.RemoveEmptyEntries);
-            string effect = split[0];
-            float v = (split.Length > 1) ? float.Parse(split[1]) : -1;
-            switch (effect)
-            {
-                case "speed":
-                    ChangeTextSpeed(v);
-                    break;
+    //private void CheckTags()
+    //{
+    //    foreach (string tag in currentTags)
+    //    {
+    //        string[] split = tag.Split(',', StringSplitOptions.RemoveEmptyEntries);
+    //        string effect = split[0];
+    //        float v = (split.Length > 1) ? float.Parse(split[1]) : -1;
+    //        switch (effect)
+    //        {
+    //            case "speed":
+    //                ChangeTextSpeed(v);
+    //                break;
 
-                case "size":
-                    ChangeTextSize(v, friendTextBox);
-                    break;
+    //            case "size":
+    //                ChangeTextSize(v, textbox);
+    //                break;
 
-                case "increasing size":
-                    this.effect = "size";
-                    value = v;
-                    break;
+    //            case "increasing size":
+    //                this.effect = "size";
+    //                value = v;
+    //                break;
 
-                default:
-                    Debug.LogError($"Text Effect Not Found: {effect}");
-                    break;
-            }
-
-
-        }
-    }
-
-    private void ChangeTextSpeed(float value)
-    {
-        if (value < 0)
-        {
-            wait = 0.15f;
-        }
-        else
-        {
-            wait = value;
-        }
-    }
-
-    private void ChangeTextSize(float value, TMP_Text currentTextbox)
-    {
-        fontSize = currentTextbox.fontSize;
-        currentTextbox.fontSize *= value;
-    }
-
-    private void IncreasingTextSize(float end, TMP_Text currentTextbox, string text, TMP_TextInfo info)
-    {
-        float start = currentTextbox.fontSize;
-        float difference = Mathf.Abs(end - start);
-
-        char[] characters = text.ToCharArray();
-        int index = 0;
-
-        TMP_MeshInfo[] cachedMeshInfo = info.CopyMeshInfoVertexData();
-        foreach (char c in characters)
-        {
-            if (Char.IsWhiteSpace(c))
-            {
-                continue;
-            }
-
-            int materialIndex = info.characterInfo[index].materialReferenceIndex;
-            Vector3[] destinationVertices = info.meshInfo[materialIndex].vertices;
-            Vector3[] sourceVertices = cachedMeshInfo[materialIndex].vertices;
-            int vertexIndex = info.characterInfo[index].vertexIndex;
+    //            default:
+    //                Debug.LogError($"Text Effect Not Found: {effect}");
+    //                break;
+    //        }
 
 
-            float charSize =  Mathf.Max(0.375f, index / (float)characters.Length);
-            print(index / (float)characters.Length);
-            Vector3 offset = (sourceVertices[vertexIndex + 0] + sourceVertices[vertexIndex + 2]) / 2;
-            destinationVertices[vertexIndex + 0] = ((sourceVertices[vertexIndex + 0] - offset) * charSize) + offset;
-            destinationVertices[vertexIndex + 1] = ((sourceVertices[vertexIndex + 1] - offset) * charSize) + offset;
-            destinationVertices[vertexIndex + 2] = ((sourceVertices[vertexIndex + 2] - offset) * charSize) + offset;
-            destinationVertices[vertexIndex + 3] = ((sourceVertices[vertexIndex + 3] - offset) * charSize) + offset;
+    //    }
+    //}
 
-            index++;
-        }
+    //private void ChangeTextSpeed(float value)
+    //{
+    //    if (value < 0)
+    //    {
+    //        wait = 0.15f;
+    //    }
+    //    else
+    //    {
+    //        wait = value;
+    //    }
+    //}
+
+    //private void ChangeTextSize(float value, TMP_Text currentTextbox)
+    //{
+    //    fontSize = currentTextbox.fontSize;
+    //    currentTextbox.fontSize *= value;
+    //}
+
+    //private void IncreasingTextSize(float end, TMP_Text currentTextbox, string text, TMP_TextInfo info)
+    //{
+    //    float start = currentTextbox.fontSize;
+    //    float difference = Mathf.Abs(end - start);
+
+    //    char[] characters = text.ToCharArray();
+    //    int index = 0;
+
+    //    TMP_MeshInfo[] cachedMeshInfo = info.CopyMeshInfoVertexData();
+    //    foreach (char c in characters)
+    //    {
+    //        if (Char.IsWhiteSpace(c))
+    //        {
+    //            continue;
+    //        }
+
+    //        int materialIndex = info.characterInfo[index].materialReferenceIndex;
+    //        Vector3[] destinationVertices = info.meshInfo[materialIndex].vertices;
+    //        Vector3[] sourceVertices = cachedMeshInfo[materialIndex].vertices;
+    //        int vertexIndex = info.characterInfo[index].vertexIndex;
 
 
-        for (int i = 0; i < info.meshInfo.Length; i++)
-        {
-            TMP_MeshInfo theInfo = info.meshInfo[i];
-            theInfo.mesh.vertices = theInfo.vertices;
-            currentTextbox.UpdateGeometry(theInfo.mesh, i);
-        }
+    //        float charSize =  Mathf.Max(0.375f, index / (float)characters.Length);
+    //        print(index / (float)characters.Length);
+    //        Vector3 offset = (sourceVertices[vertexIndex + 0] + sourceVertices[vertexIndex + 2]) / 2;
+    //        destinationVertices[vertexIndex + 0] = ((sourceVertices[vertexIndex + 0] - offset) * charSize) + offset;
+    //        destinationVertices[vertexIndex + 1] = ((sourceVertices[vertexIndex + 1] - offset) * charSize) + offset;
+    //        destinationVertices[vertexIndex + 2] = ((sourceVertices[vertexIndex + 2] - offset) * charSize) + offset;
+    //        destinationVertices[vertexIndex + 3] = ((sourceVertices[vertexIndex + 3] - offset) * charSize) + offset;
 
-    }
+    //        index++;
+    //    }
 
-    private void RestEffects(TMP_Text currentTextbox)
-    {
-        wait = 0.15f;
-        currentTextbox.fontSize = fontSize;
-    }
+
+    //    for (int i = 0; i < info.meshInfo.Length; i++)
+    //    {
+    //        TMP_MeshInfo theInfo = info.meshInfo[i];
+    //        theInfo.mesh.vertices = theInfo.vertices;
+    //        currentTextbox.UpdateGeometry(theInfo.mesh, i);
+    //    }
+
+    //}
+
+    //private void RestEffects(TMP_Text currentTextbox)
+    //{
+    //    wait = 0.15f;
+    //    currentTextbox.fontSize = fontSize;
 
     public IEnumerator incrementText(string text, TMP_Text currentTextbox)
     {
@@ -185,18 +179,6 @@ public class DialogueController : MonoBehaviour
         currentTextbox.ForceMeshUpdate();
         TMP_TextInfo textInfo = currentTextbox.textInfo;
         int totalCharacters = currentTextbox.textInfo.characterCount;
-
-        //CHECK FOR EFFECTS
-        switch (effect)
-        {
-            case "size":
-                IncreasingTextSize(value, currentTextbox, text, textInfo);
-                break;
-
-            default:
-                break;
-        }
-
 
         for (int i = 0; i < totalCharacters; i++)
         {

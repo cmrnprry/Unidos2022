065501884fee2b47f480e4932089fc2fe20ad066
book81able <ecobb81@gmail.com>
2022-10-02 10:58:36 -0400

    here

--- Assets/Scripts/LivinngRoomManager.cs
+++ Assets/Scripts/LivinngRoomManager.cs
@@ -9,18 +9,31 @@ public class LivinngRoomManager : MonoBehaviour
 
     public TextAsset ink;
     private Story story;
-    public TextMeshProUGUI textbox;
+
+    public List<GameObject> firstPlacement;
 
     // Start is called before the first frame update
     void Start()
     {
         story = new Story(ink.text);
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    // THis is hard coded im sorry
+    // So basically make a variable for the decor question
+    // and make a list of the places the player can place the decor
+    // then, set the correct one active. 
+    void readVars()
     {
-        
+        bool first = (bool) story.variablesState["FirstDecor"];
+
+        if (first)
+        {
+            firstPlacement[0].gameObject.SetActive(true);
+        }
+        else { firstPlacement[1].gameObject.SetActive(true); }
+
+
     }
 
 }

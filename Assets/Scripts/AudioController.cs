ddc561cece6b5d85d0cfb6ef1da92b9b32828a35
Cam <33362149+cmrnprry@users.noreply.github.com>
2022-10-03 17:23:58 -0400

    SETTINGS

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using DG.Tweening;
 
 public class AudioController : MonoBehaviour
 {
     public AudioClip[] musicClips;
-    public AudioSource audioSource;//ff
+    private AudioSource audioSource;//ff
     private AudioSource[] sources;//ff
+    public AudioMixer mixer;
+    private float BGMvolume;
+    private float SFXvolume;
 
     public static AudioController instance;
 
@@ -18,30 +22,46 @@ public class AudioController : MonoBehaviour
         sources = GameObject.Find("MusicSources").GetComponentsInChildren<AudioSource>();//ff
     }
 
+    public void SetSFXVolume(float vol)
+    {
+        mixer.SetFloat("SFXVolume", Mathf.Log10(vol) * 20);
+    }
+
+    public void SetBGMVolume(float vol)
+    {
+        mixer.SetFloat("BGMVolume", Mathf.Log10(vol) * 20);
+    }
+
     public void SetAudio(int index)//ff
     {
         //make this transisition smoother
 
-        foreach(AudioSource source in sources){
-          if(source.isPlaying){
-            source.Pause();
-          }
-          if(source.clip == musicClips[index]){
-            audioSource = source;
-          }
+        foreach (AudioSource source in sources)
+        {
+            if (source.isPlaying)
+            {
+                source.Pause();
+            }
+            if (source.clip == musicClips[index])
+            {
+                audioSource = source;
+            }
         }
         audioSource.Play();
     }//ff
 
-    public void PlayUIAccept(){//ff
-      GameObject.Find("UIAudio").GetComponent<AudioSource>().Play();//ff
+    public void PlayUIAccept()
+    {//ff
+        GameObject.Find("UIAudio").GetComponent<AudioSource>().Play();//ff
     }
 
-    public void PlayCameraPic(){//ff
-      GameObject.Find("CameraSFX").GetComponent<AudioSource>().Play();//ff
+    public void PlayCameraPic()
+    {//ff
+        GameObject.Find("CameraSFX").GetComponent<AudioSource>().Play();//ff
     }
 
-    public void PlaySizzle(){//ff
-      GameObject.Find("SizzleSFX").GetComponent<AudioSource>().Play();//ff
+    public void PlaySizzle()
+    {//ff
+        GameObject.Find("SizzleSFX").GetComponent<AudioSource>().Play();//ff
     }
 }
